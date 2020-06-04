bab85e5a10199c5daceea1d0201a8460820cb444
huzh <251682553@qq.com>
2020-06-04 14:31:17 +0800

    单机版注册中心

---
+++ cloud-eureka-server7001/src/main/java/com/huzh/springcloud/EurekaMain7001.java
@@ -0,0 +1,18 @@
+package com.huzh.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author huzh
+ * @description TODO
+ * @date 2020/6/4 14:23
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7001 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7001.class, args);
+    }
+}
