d286567d7b7a566c86f67d074c9e94294d0250c9
huzh <251682553@qq.com>
2020-06-05 23:05:11 +0800

    ZooKeeper服务注册

---
+++ cloud-consumerzk-order80/src/main/java/com/huzh/springcloud/OrderZKMain80.java
@@ -0,0 +1,18 @@
+package com.huzh.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author huzh
+ * @description TODO
+ * @date 2020/6/5 22:17
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class OrderZKMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderZKMain80.class, args);
+    }
+}
