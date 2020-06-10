5515e014a9c32bfc29ecd62ae5e5e72c215cc9dd
huzh <251682553@qq.com>
2020-06-10 21:57:24 +0800

    hystrix 服务熔断  服务降级  和图像化

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/huzh/springcloud/service/PaymentFallbackService.java
@@ -0,0 +1,21 @@
+package com.huzh.springcloud.service;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author huzh
+ * @description TODO
+ * @date 2020/6/10 11:47
+ */
+@Component
+public class PaymentFallbackService implements PaymentHystrixService {
+    @Override
+    public String paymentInfo_OK(Integer id) {
+        return "-----PaymentFallbackService fall back-paymentInfo_OK ,o(╥﹏╥)o";
+    }
+
+    @Override
+    public String paymentInfo_TimeOut(Integer id) {
+        return "-----PaymentFallbackService fall back-paymentInfo_TimeOut ,o(╥﹏╥)o";
+    }
+}
---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/huzh/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,18 @@
+package com.huzh.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author huzh
+ * @description TODO
+ * @date 2020/6/10 21:46
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
