7d09a6d4347bf099eb42f9f580ab27ee01415c1f
huzh <251682553@qq.com>
2020-06-11 21:50:21 +0800

    gatway 网关

---
+++ cloud-gateway-gateway9527/src/main/java/com/huzh/springcloud/GateWayMain9527.java
@@ -0,0 +1,18 @@
+package com.huzh.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author huzh
+ * @date 2020/6/11 21:18
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain9527.class, args);
+    }
+}
+
---
+++ cloud-gateway-gateway9527/src/main/java/com/huzh/springcloud/comfig/GateWayConfig.java
@@ -0,0 +1,24 @@
+package com.huzh.springcloud.comfig;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author huzh
+ * @date 2020/6/11 21:28
+ */
+@Configuration
+public class GateWayConfig {
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder routeLocatorBuilder) {
+        RouteLocatorBuilder.Builder routes = routeLocatorBuilder.routes();
+
+        routes.route("path_route_atguigu",
+                r -> r.path("/guonei")
+                        .uri("http://news.baidu.com/guonei")).build();
+
+        return routes.build();
+    }
+}
