9be37aafb861b5abc688dfa27d4b670dbd7ad8ea
251682553@qq.com <251682553@qq.com>
2020-06-27 14:41:57 +0800

    springcloud nacos配置

---
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/huzh/springcloud/OrderNacosMain83.java
@@ -0,0 +1,17 @@
+package com.huzh.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author huzh
+ * @date 2020/6/26 10:48
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class OrderNacosMain83 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderNacosMain83.class, args);
+    }
+}
---
+++ cloudalibaba-provider-payment9001/src/main/java/com/huzh/springcloud/PaymentMain9001.java
@@ -0,0 +1,17 @@
+package com.huzh.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author huzh
+ * @date 2020/6/26 10:27
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class PaymentMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9001.class, args);
+    }
+}
